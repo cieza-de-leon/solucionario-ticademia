e5bf2791edca4f24c1d36ec7854f65ebd0ed48f3
Cieza de Leon <cieza-de-leon@gmail.com>
2018-04-29 02:15:17 -0500

    Añadir solucionario de ejercicio 1 de módulo 1.

---
+++ 101.c
@@ -0,0 +1,18 @@
+/*
+ * Módulo:     1
+ * Ejercicio:  1
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+    int a, b;
+    printf("Valor del ángulo \"a\": ");
+    scanf("%d", &a);
+    printf("Valor del ángulo \"b\": ");
+    scanf("%d", &b);
+    printf("Respuesta: \x1B[32m%d\x1B[0m\n", 180 - a - b);
+
+    return 0;
+}
