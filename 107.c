ad44d01b404d033a0ab199b0475019b1b0150de4
Cieza de Leon <cieza-de-leon@gmail.com>
2018-04-29 02:40:46 -0500

    Añadir solucionario de ejercicio 7 de módulo 1.

---
+++ 107.c
@@ -0,0 +1,15 @@
+/*
+ * Módulo:     1
+ * Ejercicio:  7
+ */
+#include <stdio.h>
+
+
+int main() {
+    printf("Tres ángulos internos congruentes: \x1B[32mEquiángulo\x1B[0m\n");
+    printf("Tres ángulos internos agudos: \x1B[32mAcutángulo\x1B[0m\n");
+    printf("Un ángulo interno obtuso: \x1B[32mObtusángulo\x1B[0m\n");
+    printf("Un ángulo interno recto: \x1B[32mRectángulo\x1B[0m\n");
+
+    return 0;
+}
